2b0f202bab5df6100073160f65590a8e992f61f2
Andrey9417 <andreypovlat@gmail.com>
2021-04-16 12:12:54 +0300

    added java files

---
+++ src/package_1/Main.java
@@ -0,0 +1,14 @@
+package package_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello " + "World!");
+        Printt p = new Printt("Hello world");
+        System.out.println(p.getStr());
+    }
+}
+
+
+
---
+++ src/package_1/Printt.java
@@ -0,0 +1,13 @@
+package package_1;
+
+class Printt {
+    private String word;
+
+    Printt(String str) {
+        word = str;
+    }
+
+    public String getStr() {
+        return word;
+    }
+}
