5527a89e083b2c044d8de278cc25d753bdde918f
Andrey9417 <andreypovlat@gmail.com>
2021-04-20 01:26:38 +0300

    added Training task №1

---
+++ src/task2/Training1.java
@@ -0,0 +1,20 @@
+package task2;
+
+// 1. Write a program that multiplies 3 natural numbers and prints results to console
+
+import java.util.Scanner;
+
+public class Training1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое натуральное число");
+        int a= sc.nextInt();
+        System.out.println("Введите второе натуральное число");
+        int b= sc.nextInt();
+        System.out.println("Введите третье натуральное число");
+        int c= sc.nextInt();
+        System.out.printf("Произведение чисел: %d", a*b*c);
+    }
+}
